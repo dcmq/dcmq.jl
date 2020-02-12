79305f1c786cc25729d977325d0b833c72b40dca
vsaase <vsaase@gmail.com>
2020-02-12 15:23:55 +0100

    initial code with dscmri example

---
+++ examples/dscmri.jl
@@ -0,0 +1,62 @@
+using Pkg
+Pkg.activate(".")
+using Revise
+using DCMQ
+using DICOM
+using NIfTI
+using DSCMRI
+
+mprage_uri = Dict{String,String}()
+perfusion_uri = Dict{String,String}()
+
+function dcmhandler(channel, d, uri)
+    study_uid = d[tag"Study Instance UID"]
+    if DSCMRI.isdsc(d)
+        println("found perfusion series")
+        perfusion_uri[study_uid] = uri
+        if !(study_uid in keys(mprage_uri))
+            return
+        end
+    elseif DSCMRI.ismprage(d)
+        println("found mprage series")
+        mprage_uri[study_uid] = uri
+        if !(study_uid in keys(perfusion_uri))
+            return
+        end
+    else
+        return
+    end
+
+    filepath_perf = perfusion_uri[study_uid]
+    filepath_mprage = mprage_uri[study_uid]
+
+    println("reading file $filepath_perf")
+    nii_perf = NIfTI.niread(filepath_perf)
+    println("reading file $filepath_mprage")
+    nii_mprage = NIfTI.niread(filepath_mprage)
+    te = d[tag"Echo Time"]
+    rawvol = DSCMRI.nifti2img(nii_perf)
+    mprage = DSCMRI.nifti2img(nii_mprage)
+    println("starting perfusion tumor workflow")
+    @time perfmaps = DSCMRI.tumor_workflow(rawvol, mprage, te)
+
+    for (key, val) in perfmaps
+        vox = DSCMRI.img2nifti(val)
+        outdir = ENV["HOME"] * "/.dimseweb/nii/" * d[tag"Study Instance UID"]
+        filepath = "$outdir/$(d[tag"Series Instance UID"])_$key.nii"
+        NIfTI.niwrite(filepath, vox)
+        dcmout = copy(d)
+        dcmout[tag"Series Description"] *= " $key"
+        dcmout[tag"Image Type"][1] = "DERIVED"
+        dcmout[tag"Image Type"][2] = "SECONDARY"
+        dcmout[tag"Image Type"][3] = "RESAMPLED"
+
+        publish_nifti(channel, dcmout, filepath)
+    end
+    
+    delete!(mprage_uri, study_uid)
+    delete!(perfusion_uri, study_uid)
+    return true
+end
+
+consumer_loop("localhost", "tumorperfusion", ["stored.series.nii"], dcmhandler)
---
+++ src/DCMQ.jl
@@ -0,0 +1,71 @@
+module DCMQ
+using AMQPClient
+using DICOM
+
+export consumer_loop, publish_nifti
+
+function publish_nifti(channel, dcmout, uri)
+    if tag"Pixel Data" in keys(dcmout)
+        delete!(dcmout, tag"Pixel Data")
+    end
+    io = IOBuffer()
+    dcm_write(io, dcmout)
+    msg_out = Message(io.data, headers=Dict{String,Any}("uri" => uri))
+    basic_publish(channel, msg_out; exchange="dicom", routing_key="stored.series.nii")
+end
+
+function consumer_loop(server, queue, methods, dcmhandler; port = AMQPClient.AMQP_DEFAULT_PORT)
+    login = "guest"
+    password = "guest"
+    auth_params = Dict{String,Any}("MECHANISM"=>"AMQPLAIN", "LOGIN"=>login, "PASSWORD"=>password)
+
+    conn = connection(;virtualhost="/", host=server, port=port, auth_params=auth_params)
+    channel = channel(conn, AMQPClient.UNUSED_CHANNEL, true)
+    exchange_declare(channel, "dicom", EXCHANGE_TYPE_TOPIC)
+    queue_declare(channel, queue)
+    for method = methods
+        queue_bind(channel, queue, "dicom", method)
+    end
+
+    println("entering polling loop")
+    while true
+        maybe_msg = basic_get(channel, queue, false)
+        # check if we got a message
+        if !isnothing(maybe_msg)
+            println("message received")
+            msg = maybe_msg
+            try
+                io = IOBuffer(msg.data)
+                d = dcm_parse(io)
+                uri = extractHeaders(msg)["uri"]
+                dcmhandler(channel, d, uri)
+                basic_ack(channel, msg.delivery_tag)
+            catch e
+                println(e)
+                basic_reject(channel, msg.delivery_tag; requeue=true)
+                throw(e)
+            end
+        else
+            sleep(1)
+        end
+    end
+
+    if isopen(conn)
+        close(conn)
+        # close is an asynchronous operation. To wait for the negotiation to complete:
+        AMQPClient.wait_for_state(conn, AMQPClient.CONN_STATE_CLOSED)
+    end
+end
+
+"Return AMQP messsage headers as dictionary."
+function extractHeaders(msg::AMQPClient.Message)
+    d = Dict()
+    for x ∈ msg.properties[:headers].data
+        key = String(copy(x.name.data))
+        value = String(copy(x.val.fld.data))
+        d[key] = value
+    end
+    return d
+end
+
+end # module
